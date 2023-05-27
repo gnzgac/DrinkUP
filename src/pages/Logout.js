b0bbee548b7e47148e9ccd5f177739606cd2ecce
riru12 <brilljulianrina@gmail.com>
2023-05-27 15:39:16 +0800

    modified Login and Logout, added Signup

---
+++ src/pages/Logout.js
@@ -0,0 +1,25 @@
+import { React, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getAuth, signOut } from "firebase/auth";
+
+function Logout() {
+    let navigate = useNavigate();
+
+    useEffect(()=>{
+        const auth = getAuth();
+        signOut(auth).then(() => {
+            localStorage.setItem("isAuth", "false");
+            // Sign-out successful.
+        }).catch((error) => {
+            // An error happened.
+        });
+        
+        // navigate("/login");
+    }, [])
+
+    return (
+        <div></div>
+    )
+}
+
+export default Logout
