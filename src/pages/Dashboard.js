72765e3ec3d633449767438490f890685e8cba1d
ldpunsalan <ldpunsalan@up.edu.ph>
2023-05-28 23:15:17 +0800

    fix(dashboard): revert chase's daily display

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -106,8 +106,8 @@ function Dashboard({ isAuth }) {
               <Typography fontWeight="bold" component="h1" variant="h5" fontFamily="Segoe UI" sx = {{mb:2}} >
                 INTAKE FOR THE DAY
               </Typography>
-              <Typography>{intakeDay} mL</Typography>
-              {CircularProgressWithLabel({value:80})}
+              <Typography fontWeight="bold" fontFamily="Segoe UI" sx = {{mb:2}}>{intakeDay} mL</Typography>
+              {CircularProgressWithLabel({value:(intakeDay/2500)*100})}
             </CardContent>
           </Card>
         </Grid>
