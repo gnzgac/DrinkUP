13bf09089ca3d181f74dacdd16260bdd4865868d
ldpunsalan <ldpunsalan@up.edu.ph>
2023-06-05 11:35:07 +0800

    feat(barchart): bar chart now shows values from firebase

--- src/pages/components/BarChart.js
+++ src/pages/components/BarChart.js
@@ -1,19 +1,47 @@
 // ./components/BarChart.js
 
-import React from "react";
+import { React, useState, useEffect } from "react";
 import Chart from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 
+// Database
+import { db, auth } from '../../firebase-config';
+import { getDatabase, ref, child, get } from "firebase/database";
+
 const BarChart = () => {
-  const labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  // FEATS
+  const [intakePerDay, setIntakePerDay] = useState([]);
+  const [Date, setDate] = useState([]);
+  const dbRef = ref(db);
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  async function fetchData() {
+    // Date
+    for (let i = 0; i < 7; i++) {
+      get(child(dbRef, `testing/Days/${i}`)).then((snapshot) => {
+        if (snapshot.exists()) {
+          setDate((Date) => [...Date,snapshot.val().date]);
+          setIntakePerDay((intakePerDay) => [...intakePerDay,snapshot.val().Intake]);
+        } else {
+          console.log("No data available");
+        }
+      }).catch((error) => {
+        console.error(error);
+      });
+    }
+  }
+
   const data = {
-    labels: labels,
+    labels: Date,
     datasets: [
       {
-        label: "My First dataset",
-        backgroundColor: "rgb(255, 99, 132)",
+        label: "Your intake for the week",
+        backgroundColor: "#146C94",
         borderColor: "rgb(255, 99, 132)",
-        data: [0, 10, 5, 2, 20, 30, 45],
+        data: intakePerDay,
       },
     ],
   };
@@ -21,6 +49,7 @@ const BarChart = () => {
     <div>
       <Bar data={data} />
     </div>
+    
   );
 };
 
